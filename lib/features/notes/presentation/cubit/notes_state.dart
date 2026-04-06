7a20c4eaf89a469e4a63316cc448fa6c1f806f64
Zie <zie@Zie-MacBook-Pro.local>
2026-04-06 16:47:09 +0300

    feat: add Equatable to NotesState classes

--- lib/features/notes/presentation/cubit/notes_state.dart
+++ lib/features/notes/presentation/cubit/notes_state.dart
@@ -1,15 +1,25 @@
+import 'package:equatable/equatable.dart';
 import 'package:note_app/features/notes/domain/entities/note_entity.dart';
 
 abstract class NotesState {}
 
-class NotesLoading extends NotesState {}
-
-class NotesLoaded extends NotesState {
-  final List<NoteEntity> notes;
-  NotesLoaded(this.notes);
+class NotesLoading extends NotesState with EquatableMixin {
+  @override
+  List<Object?> get props => [];
 }
 
-class NotesError extends NotesState {
+class NotesLoaded extends NotesState with EquatableMixin {
+  final List<NoteEntity> notes;
+  NotesLoaded(this.notes);
+
+  @override
+  List<Object?> get props => [notes];
+}
+
+class NotesError extends NotesState with EquatableMixin {
   final String message;
   NotesError(this.message);
+
+  @override
+  List<Object?> get props => [message];
 }
