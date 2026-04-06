b34213f404a5d6b82658ac3694f9a0dbd3ede352
Zie <zie@Zie-MacBook-Pro.local>
2026-04-06 11:09:45 +0300

    feat: add NoteRepositoryImpl

---
+++ lib/features/notes/data/repositories/note_repository_impl.dart
@@ -0,0 +1,24 @@
+import 'package:note_app/features/notes/data/datasources/note_remote_datasource.dart';
+import 'package:note_app/features/notes/domain/entities/note_entity.dart';
+import 'package:note_app/features/notes/domain/repositories/note_repository.dart';
+
+class NoteRepositoryImpl implements NoteRepository {
+  final NoteRemoteDatasource remoteDatasource;
+
+  NoteRepositoryImpl({required this.remoteDatasource});
+
+  @override
+  Future<void> deleteNote(String id) async {
+    await remoteDatasource.deleteNote(id);
+  }
+
+  @override
+  Future<List<NoteEntity>> getAllNotes() async {
+    return await remoteDatasource.getAllNotes();
+  }
+
+  @override
+  Future<void> saveNote(NoteEntity note) async {
+    await remoteDatasource.saveNote(note);
+  }
+}
